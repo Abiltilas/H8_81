41d5f72d69979c254f1a8ac30bf53c518eaf416a
IT Production Operation Support (Abil Tilas Ramadhan) <atlas_ramadhan@yahoo.com>
2022-06-04 13:13:33 +0700

    Initial Commit add Sesi 1

---
+++ 1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+a = 4
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+
